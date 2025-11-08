d0ca0f04e481d7df4098fbd0c8920c0152047931
Felix Beer <mail@felix.beer>
2025-11-08 20:18:44 +0100

    feat: extract style into file and switch to grayscale theme

--- app/app/components/map.vue
+++ app/app/components/map.vue
@@ -4,50 +4,13 @@
       :center="center"
       :zoom="zoom"
   >
-    <MglLineLayer />
     <MglNavigationControl />
   </MglMap>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import style from '~/assets/map/style.json'
 
-const PMTILES_URL = 'https://maps.daugt.com/world.pmtiles'
-
-const style = ref({
-  version: 8,
-  sources: {
-    example_source: {
-      type: 'vector',
-      url: `pmtiles://${PMTILES_URL}`,
-      attribution: '© <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>'
-    }
-  },
-  layers: [
-    {
-      id: 'buildings',
-      source: 'example_source',
-      'source-layer': 'landuse',
-      type: 'fill',
-      paint: { 'fill-color': 'steelblue' }
-    },
-    {
-      id: 'roads',
-      source: 'example_source',
-      'source-layer': 'roads',
-      type: 'line',
-      paint: { 'line-color': 'black' }
-    },
-    {
-      id: 'mask',
-      source: 'example_source',
-      'source-layer': 'mask',
-      type: 'fill',
-      paint: { 'fill-color': 'white' }
-    }
-  ]
-})
-
-const center = [-1.559482, 47.21322]
+const center = {lat: -1.559482, lng: 47.21322}
 const zoom = 8
 </script>
