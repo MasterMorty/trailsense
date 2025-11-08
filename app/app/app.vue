6b03b8b00b9864fd28ecfd1355cac713c2392ab4
flo <{florian.sch10@gmail.com}>
2025-11-08 22:02:10 +0100

    feat: implement collapsable sidebar and center map to innsbruck

--- app/app/app.vue
+++ app/app/app.vue
@@ -1,17 +1,10 @@
-<template>
-  <div class="fixed">
-    <div v-if="status === 'authenticated'">
-      Hi there, {{ data?.user?.name }}!
-    </div>
-    <div v-else-if="status === 'unauthenticated'">
-      <button class="btn" @click="signIn('strava')">Login with Strava</button>
-    </div>
-  </div>
-
-  <Map/>
-</template>
 <script setup lang="ts">
+import Main from "~/components/ui/Main.vue";
 import Map from "~/components/map.vue";
-
-const {status, data, signIn, signOut} = useAuth()
 </script>
+<template>
+  <Main>
+    <NuxtRouteAnnouncer />
+    <Map />
+  </Main>
+</template>
